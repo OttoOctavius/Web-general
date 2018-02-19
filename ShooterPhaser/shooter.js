7af0dc74bb9012d2afe82e156e821bc71f310f6f
OttoOctavius <octaviopompilio@gmail.com>
2018-02-19 17:14:01 -0300

    Agregando estados del juego. Faltan archivos imagenes

---
+++ ShooterPhaser/shooter.js
@@ -0,0 +1,87 @@
+var cursors;
+var nave;
+
+var bullets;
+var cmd;
+
+var shooter = {
+    preload: function() {
+        game.load.image('bullet', 'assets/misc/bullet0.png');
+        game.load.audio('disparo', 'assets/digisounds/074.wav')
+    },
+
+    create: function() {
+        game.world.setBounds(0, 0, 1920, 1200);
+        game.add.sprite(0, 0, 'backdrop');
+
+        balas = game.add.audio('disparo');
+        bullets = game.add.group();
+        init_bullets(bullets);
+
+        var weapon = this.add.weapon(10, 'bullet');
+        ///weapon.fireFrom = new Phaser.Rectangle(0, 0, 10, 10);
+        weapon.autoFire = true;
+        weapon.fireRate = 600
+
+        nave = new Nave(game, 100, 180, 'nave', 100);
+        nave.animations.play('normal', 15, true);
+        nave.body.allowRotation = true;
+        nave.inputEnabled = nave.enableDrag = true;
+
+        game.camera.follow(nave);
+        cursors = game.input.keyboard.createCursorKeys();
+        game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR, Phaser.Keyboard.W]);
+
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+    },
+
+    naveacciones: function(sprite) {
+        //game.physics.arcade.angleBetween(arrow, target);
+        if (cursors.left.isDown) {
+            sprite.body.velocity.x += -5;
+        }
+        if (cursors.right.isDown) {
+            sprite.body.velocity.x += 5;
+        }
+
+        if (game.input.keyboard.isDown(Phaser.Keyboard.Q))
+            sprite.body.rotation += 1.0 / 180;
+        if (game.input.keyboard.isDown(Phaser.Keyboard.E))
+            sprite.body.rotation -= 1.0 / 180;
+
+        if (cursors.up.isDown) {
+            sprite.body.velocity.y += -10;
+        }
+        if (cursors.down.isDown) {
+            sprite.body.velocity.y += 10;
+        }
+
+        //sprite.body.velocity.x *= Math.cos(sprite.rotation);
+        if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+            balas.play();
+            fireBullet(nave);
+        }
+    },
+
+    render: function() {
+        //game.debug.cameraInfo(game.camera, 32, 32);
+        //game.debug.pointer(game.input.activePointer);
+        enemigo.forEach(function(element) {
+            game.debug.geom(element.barravida, 'rgba(255,0,0,1)');
+        }, this);
+
+        //Solo para testeo
+        jugador.forEach(function(element) {
+            temp = posUnit(element);
+            element.barravida.x = temp.x; //element.x;
+            element.barravida.y = temp.y - 10; //element.y - 10;
+            game.debug.geom(element.barravida, 'rgba(0,0,255,0.5)');
+        }, this);
+
+
+        //if (elegidos != null) {
+        //    game.debug.geom(elegidos.barravida, 'rgba(0,0,255,0.5)');
+        //}
+        game.debug.geom(puntomouse, 'rgba(0,255,0,0.25)');
+    }
+}
--- boot.js
+++ boot.js
@@ -1,21 +1,28 @@
 var boot = {
 
-	preload: function(){
-		game.load.image('nave', 'boss1.png');	
-	},
+    preload: function() {
+        game.load.image('nave', 'boss1.png');
 
-	create: function(){
-    	button = game.add.button(game.world.centerX, game.world.centerY, 'nave', onClick);
-		button.anchor.setTo(0.5);
-	},
+        //enemigos horizontales
+        game.load.image('baddie1', 'assets/sprites/shmup-baddie.png');
+        game.load.image('baddie2', 'assets/sprites/shmup-baddie2.png');
+        game.load.image('baddie3', 'assets/sprites/shmup-baddie3.png');
+        //enemigos verticales
+        game.load.image('baddie4', 'assets/sprites/shmup-ship.png');
+        game.load.image('baddie5', 'assets/sprites/shmup-ship2.png');
+        //Nave
+        game.load.spritesheet('nave', 'assets/sprites/nave32x32.png', 416 / 5, 106, 15);
+    },
 
-	update: function(){
-	}
+    create: function() {
+        button = game.add.button(game.world.centerX, game.world.centerY, 'nave', onClick);
+        button.anchor.setTo(0.5);
+    },
+
+    update: function() {}
 
 };
 
 function onClick() {
-	game.state.start('load');
-}
-
-
+    game.state.start('shooter');
+};
