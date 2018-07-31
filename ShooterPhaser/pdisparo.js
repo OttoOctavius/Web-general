7594f4c701fe3968424dd223f47e47af7fc664c1
OttoOctavius <octaviopompilio@gmail.com>
2018-07-31 09:51:42 -0300

    Mejoro animacion de nave. Falta interrupirla

--- ShooterPhaser/pdisparo.js
+++ ShooterPhaser/pdisparo.js
@@ -1,3 +1,4 @@
+//'use strict';
 var nave;
 var enemigo;
 const tiempo_estallido = 10;
@@ -21,14 +22,7 @@ var pdisparo = {
 
         bullets = game.add.group();
         init_bullets(bullets);
-        enemigo.enableBody = true;
-
-        enemigo.events.onKilled = muere;
-
-        estallido = game.add.sprite(600, 550, 'estallido');
-        estallido.animations.add('ciclo');
-        estallido.animations.add('corto', [0, 1, 2, 1, 0], 10, false);
-        estallido.play('ciclo', tiempo_estallido, true);
+        enemigobj.init();
 
         cursors = game.input.keyboard.createCursorKeys();
         game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR, Phaser.Keyboard.W, Phaser.Keyboard.E, Phaser.Keyboard.S]);
@@ -38,34 +32,60 @@ var pdisparo = {
         if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
             fireBullet(nave);
         }
-        if (enemigo.health == 0) {}
+
+        enemigobj.update();
+
         if (game.input.keyboard.justPressed(Phaser.Keyboard.E) && enemigo.time + 10000 > game.time.now) {
             enemigo.visible = true;
-            enemigo.health = 10;
-            //enemigo.heal(1); .damage(1) .alive
+            enemigo.health = 3;
+            //enemigo.heal(1); .damage(1)
         }
         if (game.input.keyboard.justPressed(Phaser.Keyboard.S)) {
             enemigo.damage(1);
         }
-
-        bullets.forEach(function(bala) {
-            game.physics.arcade.overlap(enemigo, bala, (balaa) => {
-                balaa.kill();
-                enemigo.damage(1);
-            }, null, this);
-
-        }, this);
-
+        this.render();
     },
     render: function() {
+        if (enemigo.visible) {
+            game.debug.geom(new Phaser.Rectangle(enemigo.x, enemigo.y - 10, enemigobj.anchovida(), 5), 'rgba(0,0,255,0.5)');
+        }
         game.debug.text("Tiempo del estallido:" + tiempo_estallido.toString(), 100, 50);
     }
 
-}
+};
+
+var enemigobj = {
+    init: function() {
+        enemigo.enableBody = true;
+        enemigo.events.onKilled = muere;
+
+        estallido = game.add.sprite(600, 550, 'estallido');
+        estallido.animations.add('ciclo');
+        estallido.animations.add('corto', [0, 1, 2, 1, 0], 10, true);
+        estallido.play('ciclo', tiempo_estallido, true);
+        estallido.events.onAnimationComplete.add(function(sprite, anim) { sprite.visible = false; }, this);
+    },
+    update: function() {
+        if (enemigo.alive) {
+            bullets.forEach(function(bala) {
+                game.physics.arcade.overlap(enemigo, bala, (balaa) => {
+                    bala.kill();
+                    enemigo.damage(1);
+                    estallido.x = enemigo.x;
+                    estallido.y = enemigo.y;
+                    estallido.play('corto', 10, true);
+                }, null, this);
+
+            }, this);
+        }
+    },
+    anchovida: function() {
+        return enemigo.health / 3.0 * enemigo.width;
+    }
+};
 
 function muere() {
     enemigo.loadTexture('estallido', 0);
     enemigo.animations.add('ciclo');
     enemigo.animations.play('ciclo', 30, false);
-
 }
