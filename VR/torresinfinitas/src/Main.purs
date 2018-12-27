db3338d07a631e2a9067c6b2821bbaeb291afe97
OttoOctavius <OttoOctavius@users.noreply.github.com>
2018-12-27 16:07:49 -0300

    Creado subproyecto
    purescript+BABYLON

---
+++ VR/torresinfinitas/src/Main.purs
@@ -0,0 +1,13 @@
+module Main where
+
+import Prelude
+import Effect (Effect)
+import Effect.Console (log)
+
+
+{-Correr con "pulp init"-}
+main :: Effect Unit
+main = log "Hello, World!"
+
+--main = do
+--  log "Hello sailor!"
